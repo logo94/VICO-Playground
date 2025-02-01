e0a68387c5e01c0b5b615569bc40a4ce9af57cc3
Lorenzo Gobbo <106741440+logo94@users.noreply.github.com>
2025-02-01 18:57:19 +0100

    Update toolkit.js

--- js/toolkit.js
+++ js/toolkit.js
@@ -4,4 +4,11 @@ const getRandomInt = async (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export { getRandomInt }
+const replaceHttpWithHttps = async (url) => {
+    if (url.startsWith("http://")) {
+      return url.replace("http://", "https://");
+    }
+    return url;
+}
+
+export { getRandomInt, replaceHttpWithHttps }
